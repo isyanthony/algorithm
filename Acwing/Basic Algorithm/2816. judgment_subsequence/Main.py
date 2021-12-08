a8e6a8758faed5a202d5ba92ca9267e36ad8936e
Lukose <69067909+shdsoul@users.noreply.github.com>
2021-12-08 13:59:16 +0800

    判断是否存在子序列

---
+++ Acwing/Basic Algorithm/2816. judgment_subsequence/Main.py
@@ -0,0 +1,13 @@
+
+n , m = list(map(int , input().split()))
+a = list(map(int , input().split()))
+b = list(map(int , input().split()))
+
+i = j = 0
+while i < n and j < m:
+    if a[i] == b[j]:
+        i += 1
+    j += 1
+
+if i == n : print("Yes")
+else : print("No")
