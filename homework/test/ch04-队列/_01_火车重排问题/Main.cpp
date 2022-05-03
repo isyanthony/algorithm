b6f3a34b427a7087d99b5422d8e2a09560fadf42
shdsoul <3164297994@qq.com>
2022-05-03 20:18:56 +0800

    update

--- homework/test/ch04-队列/_01_火车重排问题/Main.cpp
+++ homework/test/ch04-队列/_01_火车重排问题/Main.cpp
@@ -1,11 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+queue<int> buf[3];
+vector<int> ans;
 int main(){
-	string num , buf1 = "" , buf2 = "";
+	string num;
 	cin >> num;
-	int len = num.size() , i = 1 , j = num.size() - 1 , time = 0;
-	while(len != now){
-		
+
+	for(int i = 8 ; i >= 0 ; i--){
+		buf[2].push(num[i] - '0');
 	}
+
+	int len = num.size() , now = 0 , target = 1;
+	while(target <= 9){
+		for(int k = 0 ; k < 2 ; k++){
+			while(!buf[2].empty()){
+				int t = buf[2].front();
+				if(buf[2].front() == target){
+					ans.push_back(t);
+					buf[2].pop();
+					target++;
+				}else if(buf[k].empty() || buf[k].back() < t){
+					buf[k].push(t);
+					buf[2].pop();
+				}else break;
+			}
+		}
+
+		for(int k = 0 ; k < 3 ; k++){
+			while(buf[k].front() == target){
+				ans.push_back(target);
+				buf[k].pop();
+				target++;
+
+			}
+		}
+	}
+
+	for(int i = ans.size() - 1 ; i >= 0 ; i--){
+		cout << ans[i] << " ";
+	}
+
+
+	return 0;
 }
